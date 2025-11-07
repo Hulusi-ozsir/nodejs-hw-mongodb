c8fe4a01b4f113a80c3c22554663c5c2cc89704c
hulusi-ozsir <hulusi.ozsir@hotmail.com>
2025-11-08 00:51:56 +0300

    two

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 /* Entry point: önce DB'yi başlat, sonra sunucuyu ayağa kaldır */
-require('dotenv').config();
+require('dotenv').config({ path: __dirname + '/../.env' });
 const initMongoConnection = require('./db/initMongoConnection');
 const setupServer = require('./server');
 
 (async () => {
   try {
-    // Mongo bağlantısını kur
     await initMongoConnection();
-
-    // Sunucuyu başlat
     setupServer();
   } catch (err) {
-    // Hata varsa kapat ve logla
-    // eslint-disable-next-line no-console
     console.error('Failed to start application:', err);
     process.exit(1);
   }
